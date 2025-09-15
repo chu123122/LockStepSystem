b170b75640bb2358e436eaabe8e41f1821bd0d56
chu123122 <chu123122@LAPTOP-63KE6K7O.localdomain>
2025-09-15 20:25:04 +0800

    修正服务端代码部分bug

--- Scripts/Server/frame_sync_manager.hpp
+++ Scripts/Server/frame_sync_manager.hpp
@@ -16,18 +16,9 @@ public:
     {
         if (frameDataMap.find(frame_count) == frameDataMap.end())
         {
-            std::cout << "警告,当前帧未存在指定帧数据,请检查check_have_frame_data函数是否存在问题" << std::endl;
-            return nullptr;
+            frameDataMap[frame_count] = frameData();
         }
         return &frameDataMap[frame_count];
-    }
-
-    bool check_have_frame_data(int frame_count, int client_count)
-    {
-        // 没有指定逻辑帧的指令集
-        if (frameDataMap.find(frame_count) == frameDataMap.end())
-            return false;
-        return true;
     }
 
     void add_command_in_map(player_input_command command, const int frame_count)
@@ -42,12 +33,6 @@ public:
             player_input_command empty_command = player_input_command();
             frame_data.player_input_commands.push_back(empty_command);
         }
-    }
-
-    frameData create_empty_frameData(const int client_count)
-    {
-        frameData frame_data = frameData();
-        return frame_data;
     }
 };
 
